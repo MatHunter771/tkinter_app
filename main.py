e23fcd1966d6259abc5eb6b160accbaed21fea59
MatHunter771 <mateusz.lach@icloud.com>
2024-05-04 21:27:39 +0200

    1.2.2
    - add frames
    - segregated code

--- main.py
+++ main.py
@@ -1,66 +1,93 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class gui():
-  def __init__(self):
-      self.root = tk.Tk()
-      self.root.title("Calculator")
-      self.root.geometry("800x500")
-      self.label = tk.Label(self.root,text="Your calculation")
-      self.label.pack(padx=10,pady=10)
-      self.calculation = tk.Text(self.root,height=1)
-      self.calculation.pack(padx=10,pady=10)
-      self.button = tk.Button(self.root,text="enter",command=self.calc)
-      self.button.pack(padx=10,pady=10)
-      self.answer = tk.Button(self.root, text="ans", command=self.getAns)
-      self.answer.pack(padx=10, pady=10)
-      self.history = tk.Label(self.root, text="history:")
-      self.history.pack(padx=10,pady=10)
-      self.root.mainloop()
-  def calc(self):
-      self.calc = self.calculation.get('1.0',tk.END)
-      self.str=""
-      for i in self.calc:
-        if i == " ":
-          continue
-        elif i == "+":
-          self.value1 = float(self.str)
-          self.cm = "+"
-          self.str = ""
-          continue
-        elif i == "*":
-          self.value1 = float(self.str)
-          self.cm = "*"
-          self.str = ""
-          continue
-        elif i == "-":
-          self.value1 = float(self.str)
-          self.cm = "-"
-          self.str = ""
-          continue
-        elif i == "/":
-          self.value1 = float(self.str)
-          self.cm = "/"
-          self.str=""
-          continue
-        self.str+=i
-      self.value2 = float(self.str)
-      if self.cm == "/":
-        messagebox.showinfo(title="result",message=f"{self.value1/self.value2}")
-        self.answ = self.value1 / self.value2
-      elif self.cm == "*":
-        messagebox.showinfo(title="result",message=f"{self.value1*self.value2}")
-        self.answ = self.value1 * self.value2
-      elif self.cm == "-":
-        messagebox.showinfo(title="result",message=f"{self.value1-self.value2}")
-        self.answ = self.value1 - self.value2
-      elif self.cm == "+":
-        messagebox.showinfo(title="result",message=f"{self.value1+self.value2}")
-        self.answ = self.value1+self.value2
-      self.calculation.delete("1.0",tk.END)
-      self.calculation_history = tk.Label(self.root, text=f'{self.value1} {self.cm} {self.value2} = {self.answ}')
-      self.calculation_history.pack(padx=10,pady=10)
-  def getAns(self):
-      self.calculation.insert(tk.END,str(self.answ))
 
-gui()
+class GUI:
+
+    def __init__(self):
+        self.root = tk.Tk()
+
+        self.mainFrame = tk.Frame(self.root)
+        self.buttonsFrame = tk.Frame(self.root)
+        self.historyFrame = tk.Frame(self.root)
+
+        self.root.title("Calculator")
+        self.root.geometry("800x500")
+
+        self.label = tk.Label(self.mainFrame, text="Your calculation:")
+        self.calculation = tk.Text(self.mainFrame, height=1)
+
+        self.button = tk.Button(self.buttonsFrame, text="enter", command=self.calc)
+        self.answer = tk.Button(self.buttonsFrame, text="ans", command=self.getAns)
+
+        self.history = tk.Label(self.historyFrame, text="history:")
+
+        self.mainFrame.pack(padx=15, pady=10)
+        self.buttonsFrame.pack(padx=15, pady=10)
+        self.historyFrame.pack(side=tk.LEFT)
+
+        self.label.grid(row=0, pady=5)
+        self.calculation.grid(row=1, pady=5)
+
+        self.button.grid(row=0, column=0, padx=25)
+        self.answer.grid(row=0, column=1, padx=25)
+
+        self.history.pack()
+
+        self.root.mainloop()
+
+    def calc(self):
+        self.calc = self.calculation.get('1.0', tk.END)
+        self.str = ""
+
+        for i in self.calc:
+
+            if i == " ":
+                continue
+            elif i == "+":
+                self.value1 = float(self.str)
+                self.cm = "+"
+                self.str = ""
+                continue
+            elif i == "*":
+                self.value1 = float(self.str)
+                self.cm = "*"
+                self.str = ""
+                continue
+            elif i == "-":
+                self.value1 = float(self.str)
+                self.cm = "-"
+                self.str = ""
+                continue
+            elif i == "/":
+                self.value1 = float(self.str)
+                self.cm = "/"
+                self.str = ""
+                continue
+
+            self.str += i
+
+        self.value2 = float(self.str)
+
+        if self.cm == "/":
+            messagebox.showinfo(title="result", message=f"{self.value1 / self.value2}")
+            self.answ = self.value1 / self.value2
+        elif self.cm == "*":
+            messagebox.showinfo(title="result", message=f"{self.value1 * self.value2}")
+            self.answ = self.value1 * self.value2
+        elif self.cm == "-":
+            messagebox.showinfo(title="result", message=f"{self.value1 - self.value2}")
+            self.answ = self.value1 - self.value2
+        elif self.cm == "+":
+            messagebox.showinfo(title="result", message=f"{self.value1 + self.value2}")
+            self.answ = self.value1 + self.value2
+
+        self.calculation.delete("1.0", tk.END)
+        self.calculation_history = tk.Label(self.historyFrame, text=f'{self.value1} {self.cm} {self.value2} = {self.answ}')
+        self.calculation_history.pack(padx=10, pady=10)
+
+    def getAns(self):
+        self.calculation.insert(tk.END, str(self.answ))
+
+
+GUI()
