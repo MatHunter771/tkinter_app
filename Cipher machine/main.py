e3511a23c4d1ba6ecd372d903aded49cf48c58f8
MatHunter771 <mateusz.lach@icloud.com>
2024-05-07 23:53:50 +0200

    2.0.1
    - added elements:
    .entry textbox and header to it
    .button and prepared place to put function for it
    .shutdown protocol

--- Cipher machine/main.py
+++ Cipher machine/main.py
@@ -1,6 +1,8 @@
 import customtkinter as ctk
+from tkinter import messagebox
 
-class GUI():
+
+class GUI:
     def __init__(self):
         self.root = ctk.CTk()
         self.root.title('Cipher Machine')
@@ -9,6 +11,24 @@ class GUI():
         ctk.set_default_color_theme('dark-blue')
         ctk.set_appearance_mode('dark')
 
+        self.text = ctk.CTkLabel(self.root, text='Enter text:')
+        self.entry = ctk.CTkEntry(self.root)
+        self.enter = ctk.CTkButton(self.root, text='Enter', command=self.cypher)
+
+        self.text.pack(padx=25, pady=20)
+        self.entry.pack(padx=10, pady=10)
+        self.enter.pack()
+
+        self.root.protocol('WM_DELETE_WINDOW', self.shut_down)
+
         self.root.mainloop()
+
+    def cypher(self):
+        pass
+
+    def shut_down(self):
+        if messagebox.askyesno(title="Quit", message="Do you want to quit?"):
+            self.root.destroy()
+
 
 GUI()
